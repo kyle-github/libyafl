e7032264f2c31bd9c460f30884622e16ff69d21d
Kyle Hayes <kyle.hayes@gmail.com>
2026-01-25 10:53:26 -0800

    Perhaps we need -m32 on the assembly files too?

--- toolchains/i386-unknown-linux-gnu.cmake
+++ toolchains/i386-unknown-linux-gnu.cmake
@@ -3,4 +3,4 @@ set(ASM_FILES
     src/asm/i386/jump_i386_sysv_elf_gas.S
 )
 enable_language(ASM)
-add_compile_options(-Wall -Wextra -Werror -g -O2)
+add_compile_options(-Wall -Wextra -Werror -g -O2 -m32)
