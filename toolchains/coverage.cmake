27b2d2ef763f4c8d4e8bfab0d9c3f1711d6f9cbd
Kyle Hayes <kyle.hayes@gmail.com>
2026-02-01 14:07:31 -0800

    Checking for the wrong compiler name on macOS.

--- toolchains/coverage.cmake
+++ toolchains/coverage.cmake
@@ -6,7 +6,7 @@ if(ENABLE_COVERAGE)
         add_compile_options(-O0 -g --coverage)
         add_link_options(--coverage)
 
-    elseif(CMAKE_C_COMPILER_ID STREQUAL "Clang")
+    elseif(CMAKE_C_COMPILER_ID STREQUAL "Clang" OR CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
         message(STATUS "Enabling Clang coverage with llvm-cov")
         # Use GCC-style coverage for lcov compatibility
         add_compile_options(-O0 -g --coverage)
