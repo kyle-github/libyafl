23c1f1ed05163fb21973a91368e6dbcf50404b28
Kyle Hayes <kyle.hayes@gmail.com>
2026-05-07 12:04:48 -0700

    Removing and cleaning up unneeded files.

--- parse_workflows.py
+++
@@ -1,7 +0,0 @@
-import os, glob, yaml
-
-workflows = glob.glob('.github/workflows/*.yml')
-workflows = [w for w in workflows if 'coverage' not in w and 'update-version' not in w and 'release' not in w]
-
-# We will collect the matrix entries
-print("Parsing workflows...")
